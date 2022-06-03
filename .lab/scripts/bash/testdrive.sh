1b4b63c7b313632234ae860fed228812a049bbb3
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2022-06-03 17:15:23 +0530

    Add VM test drive script

---
+++ .lab/scripts/bash/testdrive.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+iso="$1"
+
+if [[ ! "$iso" ]]; then
+    echo "Please specify an ISO file"
+    exit 1
+fi
+
+qemu-system-x86_64 -name guest="$(basename "$iso")" \
+      -machine pc-q35-6.2,vmport=off \
+      -accel kvm \
+      -cpu host \
+      -m 2G \
+      -smp 2,sockets=2,cores=1,threads=1 \
+      -device virtio-vga-gl,xres=2560,yres=1440 \
+      -device AC97 \
+      -display sdl,gl=on,window-close=on \
+      -boot d \
+      -cdrom "$iso" \
+      -net user -nic user,model=virtio-net-pci \
+      -device virtio-keyboard-pci \
+      -device virtio-tablet-pci
+
+
