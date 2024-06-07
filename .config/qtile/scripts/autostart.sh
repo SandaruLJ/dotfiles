512ddf9d59ac07571ce6e14a04bed6ed0df5e62e
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-06-07 19:47:25 +0530

    Refactor Qtile autostart script

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Set wallpaper
+# wallpaper
 if [[ -s "$HOME/.fehbg" ]]; then
     eval "$HOME/.fehbg" 
 else
     feh --bg-fill --randomize ~/Pictures/SHARED/Wallpapers/* &
 fi
 
+# blue light filter
 redshift &
+
+# compositor
 picom -b
 
