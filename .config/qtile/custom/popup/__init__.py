6c8a3f418d6c7ea390ae48a1163a577189191397
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-06-19 11:09:23 +0530

    Fix circular import bug with Qtile popups

--- .config/qtile/custom/popup/__init__.py
+++ .config/qtile/custom/popup/__init__.py
@@ -1,4 +1,6 @@
-from .display import BrightnessSlider
+# Must be at the top since all other modules import this as well
 from .generic_popup import GenericPopup
+
+from .display import BrightnessSlider
 from .volume import MicSlider, VolumeSlider
 
