0ca477e0b5a76aed5c2d8cc58a82db76592f126b
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-07-06 13:41:06 +0530

    Refactor Hyprland effects toggle script

--- .config/hypr/scripts/toggle_effects.sh
+++ .config/hypr/scripts/toggle_effects.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# Check if animations are enabled
-FANCY_EFFECTS=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-
-if [ "$FANCY_EFFECTS" = 1 ]; then
-    # Disable effects
+disable_effects() {
     hyprctl --batch " \
         keyword animations:enabled 0; \
         keyword decoration:drop_shadow 0; \
         keyword decoration:blur:enabled 0; \
         keyword misc:vfr 1"
 
-    notify-send -u low "Effects Off" "Disabling animations and other fancy effects"
+    notify-send -eu low "Effects Off" "Disabling animations and other fancy effects"
+}
 
-    exit
+restore_effects() {
+    hyprctl reload
+    notify-send -eu low "Effects On" "Restoring configured effects"
+}
+
+if [[ $1 == "off" ]]; then
+    disable_effects
+elif [[ $1 == "on" ]]; then
+    restore_effects
+else
+    # Check if animations are enabled
+    FANCY_EFFECTS=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+
+    if [ "$FANCY_EFFECTS" = 1 ]; then
+        # Disable effects
+        disable_effects
+        exit
+    fi
+
+    # Reload with default configs
+    restore_effects
 fi
-
-# Reload with default configs
-hyprctl reload
-
-notify-send -u low "Effects On" "Restoring configured effects"
 
